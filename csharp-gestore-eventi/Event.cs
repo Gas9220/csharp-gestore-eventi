105f9e784bad1fbc282a02ac700fce067086637d
Gaspare <Gas9220@users.noreply.github.com>
2023-09-18 09:50:04 +0200

    Event class with Properties

---
+++ csharp-gestore-eventi/Event.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace csharp_gestore_eventi
+{
+    internal class Event
+    {
+        public string Title;
+        public DateTime Date;
+        public int MaxSeats;
+        public int BookedSeats;
+    }
+}
