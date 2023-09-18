d032b4609d16016bf6a2ea0ff3e217089e3c4329
Gaspare <Gas9220@users.noreply.github.com>
2023-09-18 15:12:45 +0200

    Conference class

---
+++ csharp-gestore-eventi/Conference.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace csharp_gestore_eventi
+{
+    internal class Conference : Event
+    {
+        public string Speaker { get; private set; }
+        public int Price { get; private set; }
+        public Conference(string title, DateTime date, int maxSeats, string speaker, int price) : base(title, date, maxSeats)
+        {
+            Speaker = speaker;
+            Price = price;
+        }
+
+    }
+}
