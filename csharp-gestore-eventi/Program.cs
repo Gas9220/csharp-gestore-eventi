dfd7ea626b805092af95d250edcd8a05fa2c3e77
Gaspare <Gas9220@users.noreply.github.com>
2023-09-18 15:02:53 +0200

    Code refactoring

--- csharp-gestore-eventi/Program.cs
+++ csharp-gestore-eventi/Program.cs
@@ -59,35 +59,9 @@ Console.WriteLine(eventsProgram.GetProgramInfo());
 
 string message = "";
 
-string programTitle = "";
-do
-{
-    Console.Write("What's the name of your events program? ");
-    programTitle = Console.ReadLine();
+string programTitle = checkValidString("What's the name of your events program? ", "Program must have a name");
 
-    if (isNullEmptyWhiteSpace(programTitle))
-    {
-        message = "Title can't be empty";
-        Console.WriteLine(message);
-    }
-} while (isNullEmptyWhiteSpace(programTitle));
-
-int eventsNumber;
-bool isNumber = false;
-
-do
-{
-    Console.Write("How many events would you like to add? ");
-    isNumber = Int32.TryParse(Console.ReadLine(), out eventsNumber);
-
-    if (!isNumber)
-    {
-        message = "You should insert a number";
-        Console.WriteLine(message);
-    }
-} while (!isNumber);
-
-isNumber = false;
+int eventsNumber = checkValidInt("How many events would you like to add? ", "You should insert a number");
 
 EventsProgram myEventsProgram = new EventsProgram(programTitle);
 
@@ -95,49 +69,11 @@ for (int i = 0; i < eventsNumber; i++)
 {
     Console.WriteLine($"Create {i + 1}° event");
 
-    string eventTitle = "";
+    string eventTitle = checkValidString("Event title: ", "Title can't be empty");
 
-    do
-    {
-        Console.Write("Event title: ");
-        eventTitle = Console.ReadLine();
+    DateTime eventDate = checkValidDate("Event date (dd/MM/yyyy): ", "Insert the date in a valid format");
 
-        if (isNullEmptyWhiteSpace(eventTitle))
-        {
-            message = "Title can't be empty";
-            Console.WriteLine(message);
-        }
-    } while (isNullEmptyWhiteSpace(eventTitle));
-
-    bool isDate = false;
-    DateTime eventDate;
-
-    do
-    {
-        Console.Write("Event date (dd/MM/yyyy): ");
-        isDate = DateTime.TryParse(Console.ReadLine(), out eventDate);
-
-        if (!isDate)
-        {
-            message = "Insert the date in a valid format";
-            Console.WriteLine(message);
-        }
-
-    } while (!isDate);
-
-    int eventMaxSeats = 0;
-
-    do
-    {
-        Console.Write("Event max seats: ");
-        isNumber = Int32.TryParse(Console.ReadLine(), out eventMaxSeats);
-
-        if (!isNumber)
-        {
-            message = "This field can't be a string or 0";
-            Console.WriteLine(message);
-        }
-    } while (!isNumber || eventMaxSeats == 0);
+    int eventMaxSeats = checkValidInt("Event max seats: ", "This field can't be a string or 0");
 
     myEventsProgram.AddEvent(eventTitle, eventDate, eventMaxSeats);
 }
@@ -145,35 +81,12 @@ for (int i = 0; i < eventsNumber; i++)
 Console.WriteLine($"Events count: {myEventsProgram.GetEventsCount()}");
 Console.WriteLine(myEventsProgram.GetProgramInfo());
 
-bool isDate2 = false;
-DateTime dateToSearch;
-
-do
-{
-    Console.Write("Search for an event: ");
-    isDate2 = DateTime.TryParse(Console.ReadLine(), out dateToSearch);
-
-    if (!isDate2)
-    {
-        message = "Insert the date in a valid format";
-        Console.WriteLine(message);
-    }
-
-} while (!isDate2);
+DateTime dateToSearch = checkValidDate("Search for an event: ", "Insert the date in a valid format");
 
 myEventsProgram.GetEventsOnDate(dateToSearch);
 
 myEventsProgram.ClearProgram();
 Console.WriteLine("All events have been cancelled");
-
-
-
-
-
-
-
-
-
 
 
 // Helper functions - to move in another file
@@ -186,6 +99,58 @@ bool isNullEmptyWhiteSpace(string? text)
 
     return false;
 }
+DateTime checkValidDate(string prompt, string message)
+{
+    bool isDate = false;
+    DateTime output;
+    do
+    {
+        Console.Write(prompt);
+        isDate = DateTime.TryParse(Console.ReadLine(), out output);
 
+        if (!isDate)
+        {
+            Console.WriteLine(message);
+        }
+    } while (!isDate);
+
+    return output;
+}
+
+string checkValidString(string prompt, string message)
+{
+    string output = "";
+    do
+    {
+        Console.Write(prompt);
+        output = Console.ReadLine();
+
+        if (isNullEmptyWhiteSpace(output))
+        {
+            Console.WriteLine(message);
+        }
+    } while (isNullEmptyWhiteSpace(output));
+
+    return output;
+}
+
+int checkValidInt(string prompt, string message)
+{
+    bool isNumber = false;
+    int output = 0;
+
+    do
+    {
+        Console.Write(prompt);
+        isNumber = Int32.TryParse(Console.ReadLine(), out output);
+
+        if (!isNumber)
+        {
+            Console.WriteLine(message);
+        }
+    } while (!isNumber || output == 0);
+
+    return output;
+}
 
 
