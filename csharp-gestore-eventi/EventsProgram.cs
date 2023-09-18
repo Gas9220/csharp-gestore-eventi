fe03c8a905898102f11e4c243a6271bebd24fb5a
Gaspare <Gas9220@users.noreply.github.com>
2023-09-18 11:44:58 +0200

    EventsProgram class

---
+++ csharp-gestore-eventi/EventsProgram.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace csharp_gestore_eventi
+{
+    internal class EventsProgram
+    {
+        public string title;
+        public List<Event> events;
+
+        public EventsProgram(string title)
+        {
+            this.title = title;
+            events = new List<Event>();
+        }
+    }
+}
